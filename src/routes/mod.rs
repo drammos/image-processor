709ab0af98ae758d436619d452250d8881739789
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2025-01-18 23:13:22 +0200

    Delete comment

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,5 @@
 use axum::Router;
 pub mod handler;
-// use crate::routes::{handler, user, restaurant};
-
 pub fn routes() -> Router {
     Router::new().merge(handler::routes()) // Κοινά endpoints
 }
